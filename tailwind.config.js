37fae2c7e886afcdb3f1d13a82d75464fd5caad4
Amit-TheOne <amit.dew009@gmail.com>
2023-11-22 23:47:03 +0530

    Configuration: Tailwind and folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    // './src/**/*.{html,js,jsx}',
+    './src/pages/**/*.{html,js,jsx}',
+    './src/components/**/*.{html,js,jsx}',
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
