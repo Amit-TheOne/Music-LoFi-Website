216bc23d84b1702f26bf04e0bf34a45571cd3f1c
Amit-TheOne <amit.dew009@gmail.com>
2023-11-24 23:59:53 +0530

    Created: Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // import reactLogo from './assets/react.svg
 // import viteLogo from '/vite.svg'
 
+import Navbar from "./components/Navbar";
+
 function App() {
-  return (
-   <div></div>
-  )
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
 }
 
-export default App
+export default App;
