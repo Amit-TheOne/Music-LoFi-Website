5e48410d899aee0d4a1f60e3f82111732bfdf82a
Amit-TheOne <amit.dew009@gmail.com>
2023-11-27 23:49:39 +0530

    Created: Music Streaming Page

---
+++ src/pages/Music.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Discover from "./Music/Discover";
+
+const music = () => {
+    return (
+        <div>
+            <div>
+                <Discover />
+            </div>
+        </div>
+    );
+};
+
+export default music;
