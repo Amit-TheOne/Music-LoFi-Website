9d78f8e24ea2c1323e62bd28b134f44abf4be796
Amit-TheOne <amit.dew009@gmail.com>
2023-11-27 20:38:05 +0530

    Updated: Project Folders for Home Components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Footer from "../components/Footer";
+import Navbar from "../components/Home/Navbar";
+import Hero from "../components/Home/Hero";
+import About from "../components/Home/About";
+import Footer from "../components/Home/Footer";
 
 const Home = () => {
     return (
