fb95882bc1eaf5a75de045cf2a09ad8aef671c6f
Amit-TheOne <amit.dew009@gmail.com>
2023-12-11 20:06:59 +0530

    Fixed: Minor error & bug of Route, url, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-// import reactLogo from './assets/react.svg
-// import viteLogo from '/vite.svg'
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import MusicPage from "./pages/MusicPage";
 
 function App() {
     return (
-        <BrowserRouter basename="/">
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-
-            <Routes>
-                <Route path="/music" element={<MusicPage />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/music/*" element={<MusicPage />} />
+        </Routes>
     );
 }
 
--- src/components/Music/SongCard.jsx
+++ src/components/Music/SongCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import PlayPause from "./PlayPause";
-import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { playPause, setActiveSong } from "../../redux/features/playerSlice";
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
     const dispatch = useDispatch();
@@ -21,7 +21,8 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
         <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
             <div className="relative w-full h-56 group">
                 <div
-                    className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+                    className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex"
+                    ${
                         activeSong?.title === song.title
                             ? "flex bg-black bg-opacity-70"
                             : "hidden"
