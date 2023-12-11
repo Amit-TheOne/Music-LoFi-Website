2981d3b2d8864e24507139ff1a5a9ce47e6d149e
Amit-TheOne <amit.dew009@gmail.com>
2023-12-11 20:32:16 +0530

    Updated: MusicRoute with React-router-dom

--- src/pages/MusicRoute.jsx
+++ src/pages/MusicRoute.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import Discover from "./Music/Discover";
+import { Routes, Route } from "react-router-dom";
 
 const music = () => {
     return (
-        <div>
-            <div>
-                <Discover />
-            </div>
-        </div>
+        <Routes children={true}>
+            <Route index element={<Discover />} />
+            {/* <Route path="artist" element="Helllo" /> */}
+        </Routes>
     );
 };
 
