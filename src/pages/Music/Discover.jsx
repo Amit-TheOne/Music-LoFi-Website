54230b233f82bb9172360fa24f60d261f5352f93
Amit-TheOne <amit.dew009@gmail.com>
2023-12-11 20:12:08 +0530

    Fixed: Discover data fetching

--- src/pages/Music/Discover.jsx
+++ src/pages/Music/Discover.jsx
@@ -1,45 +1,29 @@
-import React from "react";
-
-import { genres } from "../../assets/genresConstants";
-import { useGetSongsByGenreQuery } from "../../redux/services/shazamCore";
+import { useDispatch, useSelector } from "react-redux";
+import { useGetTopChartsQuery } from "../../redux/services/shazamCore";
 import Error from "../../components/Music/Error";
 import Loader from "../../components/Music/Loader";
-
-// import { Loader, Error } from "../../components/Music";
+import SongCard from "../../components/Music/SongCard";
 
 const Discover = () => {
-    const { data, isFetching, error } = useGetSongsByGenreQuery(
-        genreListId || "POP"
-    );
+    const { data, isFetching, error } = useGetTopChartsQuery();
 
-    const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
+    // const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
 
     if (isFetching) return <Loader title="Loading songs..." />;
 
     if (error) return <Error />;
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col bg-blue-900">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-3xl text-white text-left">
-                    Discover {genreTitle}
+                    Discover
                 </h2>
-
-                <select
-                    //   onChange={(e) => dispatch(selectGenreListId(e.target.value))}
-                    value={genreListId || "pop"}
-                    className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
-                >
-                    {genres.map((genre) => (
-                        <option key={genre.value} value={genre.value}>
-                            {genre.title}
-                        </option>
-                    ))}
-                </select>
             </div>
 
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-                {data?.map((song, i) => (
+                {data?.tracks?.map((song, i) => (
                     <SongCard
                         key={song.key}
                         song={song}
@@ -53,5 +37,5 @@ const Discover = () => {
         </div>
     );
 };
-
+//
 export default Discover;
